090661d42e82d6f6f9254c2eb9413b61452aa240
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-07-01 11:44:53 +0000

    adding tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -6,5 +6,20 @@ document.addEventListener("DOMContentLoaded", function() {
     // modal initialization
     let modals = document.querySelectorAll('.modal');
     M.Modal.init(modals);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
     });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
+
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
+});
 
